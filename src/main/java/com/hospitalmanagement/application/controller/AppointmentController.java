6486eb99988a5646d0a0509e725c846b962a54b4
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-01-04 11:37:55 +0100

    appointment controller and service

---
+++ src/main/java/com/hospitalmanagement/application/controller/AppointmentController.java
@@ -0,0 +1,12 @@
+package com.hospitalmanagement.application.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/appointment")
+public class AppointmentController {
+
+
+}
